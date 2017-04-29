506c4901a8f2df0f748cfb9d4ace28e6dbbe12ed
mclinch <michael.clinch@gmail.com>
2017-04-29 13:46:10 -0700

    should be final

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,9 +6,19 @@ $(document).ready(function(){
   var wins = 0;
   var losses = 0;
   var guesses = 9;
+  var userPastGuess = [];
 
- 
-var chooseLetter = letters[Math.floor(Math.random() * letters.length)]
+  var chooseLetter = letters[Math.floor(Math.random() * letters.length)];
+
+  function reset() {
+    chooseLetter = letters[Math.floor(Math.random() * letters.length)];
+    guesses = 9;
+    userGuess = [];
+    userPastGuess = [];
+    document.getElementById('user-guess').innerHTML = " ";
+  }
+
+  
   console.log(chooseLetter)
 
 
@@ -17,22 +27,37 @@ document.onkeypress = function(event) {
     var userGuess = event.key;
 
     if(userGuess === chooseLetter){
+        reset();
         wins++;
+        alert("Your a Psychic! You win!")
+        console.log(chooseLetter)
     } else {
+    if (userPastGuess.indexOf(userGuess) >= 0) {
+      alert("Please Choose another Letter.")
+      return;
+    }    
+    userPastGuess.push(userGuess);
+    console.log(userPastGuess);
+
+
+    
+    if (guesses > 0) {
         guesses--;
-      }
+        // letters.push(userGuess);
+    }
 
     if(guesses === 0){
         losses++;
-        guesses = 9;
-        
+        reset();
+        console.log(chooseLetter)
+     
 
-    }
+    }}
    
     document.getElementById('wins').innerHTML = "Wins: " + wins;
-    document.getElementById('losses').innerHTML = "losses: " + losses;
+    document.getElementById('losses').innerHTML = "Losses: " + losses;
     document.getElementById('guessesLeft').innerHTML = "Guesses left: " + guesses;
-    document.getElementById('chosen').innerHTML = "Letters you have chosen: " + userGuess;
+    document.getElementById('user-guess').innerHTML = userPastGuess.join(', ');
 }
 
 
